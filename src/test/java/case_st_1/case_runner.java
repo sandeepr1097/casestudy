6eff52b2bce535513e9a8915d69b5c0af664b3b7
Training_C2A.04.29 <Training_C2A.04.29@accenture.com>
2019-09-06 14:38:25 +0530

    case_study

---
+++ src/test/java/case_st_1/case_runner.java
@@ -0,0 +1,12 @@
+package case_st_1;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome=true)
+public class case_runner {
+
+}
